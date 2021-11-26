daedd5bc22cbb54d7f1e9cc4de7b53e276caa035
cleeao <cleeao@connect.ust.hk>
2021-11-26 22:05:31 +0000

    can check if 2K launcher is running

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import process_checker
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +14,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+    print(process_checker.check_if_process_is_running("t2gp.exe"))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ process_checker.py
@@ -0,0 +1,15 @@
+# to check what processes are running
+import psutil as psutil
+
+
+def check_what_processes_are_running():
+    processes_list = []
+    for proc in psutil.process_iter():
+        processes_list.append(proc.name())
+    return processes_list
+
+def check_if_process_is_running(process_name):
+    for proc in psutil.process_iter():
+        if proc.name().lower() == process_name.lower():
+            return True
+    return False

5142a8b68183ebf65ce2f252e1339992a87a6f43
User <sample@email.com>
2021-12-25 05:06:55 +0000

    changed react_to_time to say things

--- time_monitor.py
+++ time_monitor.py
@@ -1,24 +1,32 @@
 # To monitor consecutive playtime
 import os  # just for the next line
-import time
-
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"  # Stop pygame from displaying welcoming message
 from pygame import mixer  # Need it just to play sound
+import text_to_speech
 
 
 def react_to_time(minutes):
     if minutes == 15:
-        print("You have been playing for 15 minutes!")
-        make_ding_sound()
+        say_and_print("You have been playing for 15 minutes")
     if minutes == 30:
-        print("You have been playing for 30 minutes")
-        make_ding_sound()
+        say_and_print("You have been playing for 30 minutes")
     if minutes > 30:
-        print(f"You have been playing for {minutes} minutes")
-        make_ding_sound()
+        say_and_print(f"You have been playing for {minutes} minutes")
 
 
 def make_ding_sound():
     mixer.init()
     mixer.music.load("audio_files/ding-sound-effect_2.mp3")
     mixer.music.play()
+
+
+def say(text):
+    path = text_to_speech.get_mp3_path(text)
+    mixer.init()
+    mixer.music.load(path)
+    mixer.music.play()
+
+
+def say_and_print(text):
+    print(text)
+    say(text)
